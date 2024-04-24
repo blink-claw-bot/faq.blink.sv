a47de038442669b02f7e5b190d5ca2003b317c3a
openoms <43343391+openoms@users.noreply.github.com>
2024-04-24 13:23:44 +0200

    feat: add nix flake and blink-bot-sync.py (#84)

---
+++ blink-bot-sync.py
@@ -0,0 +1,107 @@
+import os
+import logging
+from dotenv import load_dotenv
+from llama_index.core import Settings, StorageContext, VectorStoreIndex
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.vector_stores.pinecone import PineconeVectorStore
+from pinecone import Pinecone
+from llama_index.readers.github import GithubRepositoryReader, GithubClient
+import nest_asyncio
+from pyairtable import Api
+from llama_index.core.schema import TextNode
+
+# Configure basic logging to the console with an informational level
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.info("Loading environment variables")
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("GALOY_OPENAI_API_KEY")
+OPENAI_ORG_ID = os.getenv("GALOY_OPENAI_ORG_ID")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+AIRTABLE_ACCESS_TOKEN = os.getenv("AIRTABLE_ACCESS_TOKEN")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+# Create RAG Index
+logging.info("Create RAG Index")
+Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-large", api_key=OPENAI_API_KEY, organization=OPENAI_ORG_ID)
+vector_store = PineconeVectorStore(index_name="blink-3072")
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+pc = Pinecone(api_key=PINECONE_API_KEY)
+INDEX = "blink-3072"
+pc.describe_index(INDEX)
+
+# Load FAQ
+logging.info("Load FAQ")
+github_client = GithubClient(GITHUB_TOKEN)
+repo_owner = "GaloyMoney"
+repo_name = "faq.blink.sv"
+repo_branch = "main"
+loader = GithubRepositoryReader(
+    github_client,
+    owner=repo_owner,
+    repo=repo_name,
+    filter_file_extensions=([".md"], GithubRepositoryReader.FilterType.INCLUDE),
+    filter_directories=(["i18n", ".github", ".gitbook"], GithubRepositoryReader.FilterType.EXCLUDE),
+    verbose=False
+)
+nest_asyncio.apply()
+md_docs = loader.load_data(branch=repo_branch)
+logging.info(f"Loaded {len(md_docs)} documents from GitHub.")
+
+# Filter documents
+for d in md_docs:
+    if d.metadata["file_path"] == "SUMMARY.md":
+        md_docs.remove(d)
+        continue
+    d.id_ = d.metadata["file_path"]
+    d.metadata["source"] = "FAQ"
+    d.metadata["language"] = "EN"
+    d.metadata["extension"] = ".md"
+    d.excluded_embed_metadata_keys = ["url", "source", "language", "file_name", "extension"]
+    d.excluded_llm_metadata_keys = ["url", "source", "language", "file_name", "extension"]
+logging.info(f"Remaining documents after filtering: {len(md_docs)}")
+
+# Load AirTable
+logging.info("Load AirTable")
+api = Api(AIRTABLE_ACCESS_TOKEN)
+table = api.table('appHWT7d7zX6k0gXo', 'tblDSw7q7gjH7CAHo')
+records = table.all()
+logging.info(f"Fetched {len(records)} records from Airtable.")
+
+# Prepare QA nodes for indexing
+qa_nodes = []
+for record in records:
+    text = f"- User: {record['fields']['Question']}\n- Assistant: {record['fields']['Answer']}"
+    metadata = {
+        'language': record['fields']['Language'],
+        'source': "Support QAs"
+    }
+    node = TextNode(text=text, id_=record['id'], metadata=metadata, excluded_embed_metadata_keys=['language', 'source'])
+    qa_nodes.append(node)
+logging.info(f"Prepared {len(qa_nodes)} QA nodes for indexing.")
+
+assert len(qa_nodes) == len(records)
+
+# Review / delete existing
+logging.info("Review / delete existing index")
+index = pc.Index(INDEX)
+index.describe_index_stats()
+index.delete(delete_all=True)
+logging.info(f"Deleted the contents of {INDEX}.")
+
+# Upload indexes from the FAQs
+logging.info(f"Initializing upload from FAQs with {len(md_docs)} documents.")
+index = VectorStoreIndex.from_documents(
+    md_docs,
+    storage_context=storage_context
+)
+logging.info("Uploading from the FAQs is completed.")
+
+# Upload indexes from the AirTable
+logging.info(f"Initializing upload from the Airtable with {len(qa_nodes)} documents.")
+index = VectorStoreIndex(
+    qa_nodes,
+    storage_context=storage_context
+)
+logging.info("Uploading from the AirTable is completed.")
---
+++ flake.nix
@@ -0,0 +1,85 @@
+{
+  description = "Python environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"] (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [];
+      };
+      pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+        ipykernel
+        ipython
+        ipywidgets
+        llama-index
+        openai
+        pandas
+        python-dotenv
+        requests
+        (ps.buildPythonPackage rec {
+          pname = "llama-index-vector-stores-pinecone";
+          version = "0.1.4";
+          format = "pyproject";
+          src = pkgs.fetchurl {
+            url = "https://files.pythonhosted.org/packages/15/7a/772a2ee448c0d8e199ab21ceec71abee2e956b702eca9fbec867478b6e4f/llama_index_vector_stores_pinecone-0.1.4.tar.gz";
+            sha256 = "ab5f2141d44404c9ad36611c11e8b6afb35b6f0a80959726f8eab5b65a157549";
+          };
+          nativeBuildInputs = [ poetry-core ];
+          propagatedBuildInputs = [
+            llama-index-core
+            pinecone-client
+          ];
+        })
+        (ps.buildPythonPackage rec {
+          pname = "llama-index-readers-github";
+          version = "0.1.7";
+          format = "pyproject";
+          src = pkgs.fetchurl {
+            url = "https://files.pythonhosted.org/packages/b5/11/afae7d3dc8e03144ae318a8be713a44f184b53a63414ed13a8c62311ecc1/llama_index_readers_github-0.1.7.tar.gz";
+            sha256 = "5dc8d2d10615640da75e0bea03147795a5e752556e9dc701852c1a6b182b869d";
+          };
+          nativeBuildInputs = [ poetry-core ];
+          propagatedBuildInputs = [
+            httpx
+            llama-index-core
+            llama-index-readers-file
+          ];
+        })
+        (ps.buildPythonPackage rec {
+          pname = "pyairtable";
+          version = "2.3.3";
+          src = pkgs.fetchPypi {
+            inherit pname version;
+            sha256 = "6f52bd270d28ba6459b7cc473588e517085a737d70c68e855e6d104972e1121e";
+          };
+        propagatedBuildInputs = with pkgs.python3Packages; [
+          requests
+          typing-extensions
+          inflection
+          pydantic
+        ];
+        checkInputs = with pkgs.python3Packages; [
+          pytest
+          requests-mock
+          mock
+          responses
+        ];
+        doCheck = false;
+      })
+      ]);
+    in {
+      packages.default = pythonEnv;
+      devShells.default = pkgs.mkShell {
+        buildInputs = [ pythonEnv ];
+        TIKTOKEN_CACHE_DIR = ".cache/.tiktoken";
+        shellHook = ''
+          echo "The Python environment is ready."
+        '';
+      };
+    });
+}
